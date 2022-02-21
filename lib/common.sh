8780a2a1ab418559f5b1031a8e8353d81189ea19
andersmurphy <andersmurphy@gmail.com>
2022-02-21 21:54:19 +0000

    Pull out export_env_dir into common

--- lib/common.sh
+++ lib/common.sh
@@ -2,6 +2,19 @@
 
 export BUILDPACK_STDLIB_URL="https://lang-common.s3.amazonaws.com/buildpack-stdlib/v7/stdlib.sh"
 
+export_env_dir() {
+  env_dir=$1
+  acceptlist_regex=${2:-''}
+  denylist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
+  if [ -d "$env_dir" ]; then
+    for e in $(ls $env_dir); do
+      echo "$e" | grep -E "$acceptlist_regex" | grep -qvE "$denylist_regex" &&
+      export "$e=$(cat $env_dir/$e)"
+      :
+    done
+  fi
+}
+
 cache_copy() {
   rel_dir=$1
   from_dir=$2
